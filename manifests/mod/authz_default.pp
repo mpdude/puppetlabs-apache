b65b7874a3ab352d47864c39ec5ae5eb135e06d7
Matthias Pigulla <mp@webfactory.de>
2015-08-18 23:37:43 +0200

    Catch that mod_authz_default has been removed in Apache 2.4
    Having it in the server config will prevent the server from starting as the module file cannot be found.

--- manifests/mod/authz_default.pp
+++ manifests/mod/authz_default.pp
@@ -1,3 +1,7 @@
 class apache::mod::authz_default {
-  ::apache::mod { 'authz_default': }
+  if versioncmp($apache_version, '2.4') >= 0 {
+    warning('apache::mod::authz_default has been removed in Apache 2.4')
+  } else {
+    ::apache::mod { 'authz_default': }
+  }
 }
